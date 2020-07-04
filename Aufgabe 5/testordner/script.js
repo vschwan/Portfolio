30ecaabdf51bd62dbbacf7ee67d2f993ef40ccda
vschwan <schwanvalentina0@gmail.com>
2020-07-04 19:37:53 +0200

    update

--- Aufgabe 5/testordner/script.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var emissionAsia = 1000;
-var emissionEurope = 100;
-var totalEmission = emissionAsia + emissionEurope;
-console.log("Die Gesamtemission ist: " + totalEmission);
-//# sourceMappingURL=script.js.map
--- Aufgabe 5/testordner/script.ts
+++
@@ -1,9 +0,0 @@
-var emissionAsia:number = 1000;
-
-var emissionEurope:number = 100;
-
-var totalEmission:number = emissionAsia + emissionEurope;
-
-console.log("Die Gesamtemission ist: " + totalEmission);
-
-
